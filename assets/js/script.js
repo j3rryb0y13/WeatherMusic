1a0d873fe20419a219fb9d2d1de88e7a1382325c
Ameer Clark <ameerclark18@gmail.com>
2024-04-11 20:40:26 -0400

    played with the console log

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,21 @@ const APIkey = "46d353ce29765dbac0983953dafd4b19"
 
 $("form").on("submit",function(event){
     event.preventDefault();
-    this.reset()
-
+    
     
     let city = event.target[0].value;
     const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
-
+    
     fetch(queryURL)
     .then(response => {
-    return response.json()
+        return response.json()
     })
     .then(data => generateWeather(data))
     .catch(error => console.log('ERROR'))
     localStorage.setItem('cityname', city)
+
+    
+    this.reset();
 })
 
 function generateWeather(weather){
