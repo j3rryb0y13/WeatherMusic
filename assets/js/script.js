8a18c72a93122853d7abbf305273400611a5a830
Ameer Clark <ameerclark18@gmail.com>
2024-04-15 19:52:59 -0400

    functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,45 @@
-
+const APIkey = "46d353ce29765dbac0983953dafd4b19"
 
 $("form").on("submit",function(event){
     event.preventDefault();
-    this.reset()
-
+    
     
     let city = event.target[0].value;
     const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
-
+    
     fetch(queryURL)
     .then(response => {
-    return response.json()
+        return response.json()
     })
     .then(data => generateWeather(data))
-    .catch(error => console.log('ERROR'))
+   // .catch(error => console.log('ERROR'))
     localStorage.setItem('cityname', city)
+
+    
+    this.reset();
 })
 
 function generateWeather(weather){
     console.log(weather)
+const card =  $('<div>').addClass('card')
+const cardBody = $('<div>').addClass('card-body')
+   
     
+   const weatherName = $('<h2>').text(weather.name)
+   const weatherDesc = $('<h3>').text(weather.weather[0].description)
+   const weatherTemp = $('<p>').text(weather.main.temp)
+
+   weatherName.addClass('card-title') 
+   weatherDesc.addClass('card-title')
+   weatherTemp.addClass('card-text')
+
+   $(cardBody).append(weatherName)
+   $(cardBody).append(weatherDesc)
+   $(cardBody).append(weatherTemp)
+
+
+    $(card).append(cardBody)
+    $('#weather-container').append(card)
 }
 
 
